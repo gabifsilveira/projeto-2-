35d2c837a9ac0aef6ccf4862746de2de62f18389
gabifsilveira <160637618+gabifsilveira@users.noreply.github.com>
2025-05-23 19:17:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'história',
+    'O que é PQuem foi a rainha do antigo Egito conhecida por sua beleza e sua aliança com Júlio César?',
+    'Cleópatra'
+)
+
+criaCartao(
+    'Sociologia',
+    'Segundo Émile Durkheim (1858-1917), quais as três principais características do Fato Social?',
+    'generalidade, a exterioridade e a coercitividade'
+)
+
+criaCartao(
+    'biologia',
+    'Espermatozoide é a célula reprodutiva...',
+    'masculina de todos os animais que se reproduzem a partir de reprodução sexuada'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
